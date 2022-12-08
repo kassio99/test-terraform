d32e249b4c80cc64058aea045d6040b0480b078b
Kassio Ogliari Ruaro <kassio_99@hotmail.com>
2022-12-08 19:13:04 -0300

    Add Extras

---
+++ files/userdata.sh
@@ -0,0 +1,4 @@
+sudo amazon-linux-extras enable nginx1
+sudo amazon-linux-extras install -y nginx1
+systemctl start nginx
+systemctl enable nginx
--- main.tf
+++ main.tf
@@ -1,8 +1,28 @@
+data "aws_ami" "amazon2" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20221103.3-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "web-test" {
   for_each                = toset(var.int_names)
-  ami                     = var.amis[var.region]
+  ami                     = data.aws_ami.amazon2.id
   instance_type           = var.int_type
   disable_api_termination = var.disable_api_termination
+  user_data               = file("./files/userdata.sh")
+  key_name                = "ChaveServidor"
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
 
   tags = {
     Name = each.key
@@ -14,7 +34,7 @@ resource "aws_s3_bucket" "s3-kassio-test" {
   acl    = "private"
 }
 
-resource "aws_sqs_queue" "terraform_queue" {
+resource "aws_sqs_queue" "terraform_test" {
   name                      = "terraform--queue"
   delay_seconds             = 90
   max_message_size          = 2048
