88d913a1bb1e608987f0a70e1e22dfc45f77e2f2
Kassio Ogliari Ruaro <kassio_99@hotmail.com>
2022-12-08 20:21:37 -0300

    Add SG

--- main.tf
+++ main.tf
@@ -29,6 +29,50 @@ resource "aws_instance" "web-test" {
   }
 }
 
+resource "aws_security_group" "test_sg" {
+  name   = "sgservertest"
+  vpc_id = "vpc-038a96a45cc7e0a92"
+
+  ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["177.155.206.82/32"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["177.155.206.82/32"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["177.155.206.82/32"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "sgservertest"
+  }
+}
+
 resource "aws_s3_bucket" "s3-kassio-test" {
   bucket = "kassio99-test"
   acl    = "private"
